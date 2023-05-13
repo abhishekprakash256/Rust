d934e1e37da9c15f003c3e57ad552c5edbfbd014
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-05-12 20:35:01 -0400

    structs code

--- The-Book/structs.rs
+++ The-Book/structs.rs
@@ -1,2 +1,50 @@
 // the structs in rust
 
+
+struct User 
+{
+	username: String,
+	email: String,
+	id : u32,
+	active: bool,
+}
+
+
+fn main()
+{ 
+
+	let User_1 = User
+	{
+		username: String::from("Abhi"),
+		email: String::from("abhi@gmail.com"),
+		id: 5,
+		active: true
+	};
+
+	let mut User_2 = User
+	{
+		username: String::from("Abhi"),
+		email: String::from("anny@gmail.com"),
+		id:2,
+		active: true
+	};
+
+	let username_2 = String::from("Anny");
+	User_2.username = username_2;
+
+	let User_3 = User
+	{
+		username: String::from("AbhiAnny"),
+		email: String::from("abhianny25@gmail.com"),
+		..User_2
+
+	};
+
+
+	
+	println!("the username is {}",User_1.username);
+
+	println!("the username is {}", User_2.username);
+
+	println!("the username is {}", User_3.username);
+}
